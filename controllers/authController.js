ebbf48cc6f33b5b46e779a7a0c0ac44da526c8fd
Yangyin Xu <37276487+yangyinxu@users.noreply.github.com>
2022-06-28 08:40:07 -0400

    Added login capability.
    Added json web token for validation.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const user = require('../models/user');
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -37,6 +39,53 @@ exports.signup = (req, res, next) => {
         .catch(error => {
             if (!error.statusCode) {
                 error.statusCode = 500;
+            }
+            next(error)
+        });
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user with this email could not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password!');
+                error.status = 401;
+                throw error;
+            }
+            // generate a new token via jason web token
+            // Note: instead of using `secret` as the private key
+            // for sigining, a much longer/complicated string should
+            // be used in a real application.
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                },
+                'secret',
+                {
+                    expiresIn: '1h'
+                }
+            );
+            res.status(200).json({
+                token: token,
+                userId: loadedUser._id.toString()
+            });
+        })
+        .catch(error => {
+            if (!error.statusCode) {
+                error.satusCode = 500;
             }
             next(error)
         });
